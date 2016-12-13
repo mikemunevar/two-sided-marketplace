e1ba75222a292e8cf18c61603dd32d9743bc512b
mikemunevar <mikemunevar@gmail.com>
2016-12-13 01:40:42 +0000

    Improved the view for products, addresses, and stores.

--- app/helpers/stores_helper.rb
+++ app/helpers/stores_helper.rb
@@ -1,2 +1,23 @@
 module StoresHelper
+    
+    def edit_address_link(address)
+        link_to("Edit This Address", edit_address_path(address) )
+    end    
+
+    def show_address_link(address, name)
+        link_to(name, address_path(user) )
+    end
+
 end
+
+
+# module UsersHelper
+    
+#     def edit_user_link(user)
+#         link_to("Edit This User", edit_user_path(user) )
+#     end    
+
+#     def show_user_link(user, title)
+#         link_to(title, user_path(user) )
+#     end
+# end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+    
+    def edit_user_link(user)
+        link_to("Edit This User", edit_user_path(user) )
+    end    
+
+    def show_user_link(user, first_name)
+        link_to(first_name, user_path(user) )
+    end
 end
